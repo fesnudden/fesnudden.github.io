7fcb41a47767303df5498f6e686e07f5652c2cec
Fenella Snudden <fvsnudden1@sheffield.ac.uk>
2020-07-08 17:37:55 +0100

    Initial commit

---
+++ js/interactive.js
@@ -0,0 +1,24 @@
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
+$(document).ready(function () {
+
+  $('.first-button').on('click', function () {
+
+    $('.animated-icon1').toggleClass('open');
+  });
+  $('.second-button').on('click', function () {
+
+    $('.animated-icon2').toggleClass('open');
+  });
+  $('.third-button').on('click', function () {
+
+    $('.animated-icon3').toggleClass('open');
+  });
+});
